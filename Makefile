3ecc44e8eaad07c6c8b2a006d8e87c696521bb70
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-04-24 16:00:14 -0400

    Removed excess CUnit files

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ vector.o: src/linear/vector.cu
 	nvcc -c src/linear/vector.cu -arch=sm_21
 
 CHip.o: src/CHip.c
-	gcc -c src/CHip.c
+	clang -c src/CHip.c
 
 CHip: CHip.o vector.o matrix.o
 	nvcc CHip.o vector.o matrix.o -arch=sm_21 -lcudart -O3 -o CHip
--- src/linear/matrix.cu
+++ src/linear/matrix.cu
@@ -1,5 +1,32 @@
 #include "matrix_functions.cu"
+#include <stdlib.h>
 
 extern "C" {
 	#include "matrix.h"
+}
+
+extern "C"
+c_matrix *new_c_matrix(int i, int j) {
+	c_matrix *m = (c_matrix *)malloc(sizeof(*m));
+	if(m == NULL)
+		return NULL;
+
+	m->data = (double *)malloc(sizeof(double) * i * j);
+	if(m->data == NULL) {
+		free(m);
+		return NULL;
+	}
+
+	m->n = i;
+	m->m = j;
+
+	return m;
+}
+
+extern "C"
+void del_c_matrix(c_matrix *m) {
+	if( m != NULL ) {
+		free(m->data);
+		free(m);
+	}
 }
