b78570114b58916d3d27b1638ee02e5017124c05
Joseph Barrow <joseph.d.barrow@gmail.com>
2014-04-25 21:01:05 -0400

    Added timer code

---
+++ src/timer.h
@@ -0,0 +1,20 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <stdlib.h>
+#include <sys/time.h>
+
+struct timeval timerStart;
+
+void start_time() {
+	gettimeofdat(&timerStart, NULL);
+}
+
+double get_time() {
+	struct timeval timerStop, timerElapsed;
+	gettimeofday(&timerStop, NULL);
+	timersub(&timerStop, &timerStart, &timerElapsed);
+	return timerElapsed.tv_sec*1000.0 + timerElapsed.tv_usec/1000.0;
+}
+
+#endif
